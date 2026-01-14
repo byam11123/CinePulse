298b236fea8888c34b5a3a24431651be52739765
byam11123 <kaiwartyabyamkesh7@gmail.com>
2026-01-14 13:18:23 +0530

    frontend files, signup & login ui done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
+  );
+}
+
+export default App;

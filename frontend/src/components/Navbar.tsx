689d58a8b8ca6379f50467db3840e45b6e371b8a
byam11123 <kaiwartyabyamkesh7@gmail.com>
2026-01-16 16:54:07 +0530

    fixed the lucide issue

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { LogOut, Menu, Search } from "lucide-react";
+import { LogOut, Search, Menu } from "lucide-react";
 import { useAuthStore } from "../store/authUser.js";
-
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const { user, logout } = useAuthStore();
-
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
   return (
-    <header className="relative max-w-5xl mx-auto flex items-center justify-between p-4 h-20 ">
+    <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
       <div className="flex items-center gap-10 z-50">
         <Link to={"/"}>
           <img
@@ -20,7 +18,7 @@ const Navbar = () => {
             className="w-32 sm:w-40"
           />
         </Link>
-        {/* desktop navbar items*/}
+        {/*desktop navbar items*/}
         <div className="hidden sm:flex gap-2 items-center">
           <Link to={"/"} className="hover:underline">
             Movies
@@ -28,7 +26,7 @@ const Navbar = () => {
           <Link to={"/"} className="hover:underline">
             TV Shows
           </Link>
-          <Link to="/history" className="hover:underline">
+          <Link to={"/history"} className="hover:underline">
             Search History
           </Link>
         </div>
@@ -43,45 +41,36 @@ const Navbar = () => {
           className="h-8 rounded cursor-pointer"
         />
         <LogOut className="size-6 cursor-pointer" onClick={logout} />
-
         <div className="sm:hidden">
           <Menu className="size-6 cursor-pointer" onClick={toggleMobileMenu} />
         </div>
       </div>
-
-      {/* mobile nav items */}
+      {/* mobile navbar items*/}
       {isMobileMenuOpen && (
-        <div className="absolute top-full left-0 w-full sm:hidden mt-7 z-40 bg-black border border-gray-800 rounded">
+        <div className="w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800">
           <Link
             to={"/"}
-            className="block p-3 hover:bg-gray-800"
+            className="block hover:underline p-2"
             onClick={toggleMobileMenu}
           >
             Movies
           </Link>
           <Link
             to={"/"}
-            className="block p-3 hover:bg-gray-800"
+            className="block hover:underline p-2"
             onClick={toggleMobileMenu}
           >
             TV Shows
           </Link>
           <Link
-            to="/history"
-            className="block p-3 hover:bg-gray-800"
+            to={"/"}
+            className="block hover:underline p-2"
             onClick={toggleMobileMenu}
           >
             Search History
           </Link>
         </div>
       )}
-
-      {/* <div>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/"}>Movies</Link>
-          <Link to={"/"}>TV Shows</Link>
-          <Link to="/history">Search History</Link>
-        </div> */}
     </header>
   );
 };
